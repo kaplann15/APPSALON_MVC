4c83a565d1c20b4dfc855a3d0f6d71c82a6f6a34
kaplann15 <josuetorres@MacBook-Pro-de-Josue-3.local>
2023-04-05 19:53:10 -0600

    arreglando correo espacio en token

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -141,19 +141,19 @@ class ActiveRecord {
     public function crear() {
         // Sanitizar los datos
         $atributos = $this->sanitizarAtributos();
-
+     
         // Insertar en la base de datos
         $query = " INSERT INTO " . static::$tabla . " ( ";
         $query .= join(', ', array_keys($atributos));
-        $query .= " ) VALUES (' "; 
+        $query .= " ) VALUES ('"; 
         $query .= join("', '", array_values($atributos));
-        $query .= " ') ";
-        
+        $query .= "') ";
+     
         // Resultado de la consulta
         $resultado = self::$db->query($query);
         return [
-           'resultado' =>  $resultado,
-           'id' => self::$db->insert_id
+            'resultado' =>  $resultado,
+            'id' => self::$db->insert_id
         ];
     }
 
